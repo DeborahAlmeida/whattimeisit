69a31cac9813f801f0cd014d421653697cfefc31
Deborah Almeida <deborahsoares01@gmail.com>
2020-02-12 12:15:07 -0300

    add image folder

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function carregar (){
     msg.innerHTML = ` Agora são ${hora}h e ${minute} minutos `
     if (hora >= 0 && hora < 12){
         // BOM DIA 
-        img.src = "img_dia.jpg"
+        img.src = "IMAGES/img_dia.jpg"
         document.body.style.background = "rgb(220,185,119)"
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde
-        img.src = "img_tarde.jpg"
+        img.src = "IMAGES/img_tarde.jpg"
         document.body.style.background = "rgb(203, 126, 98)"
     } else {
         //BOA NOITE 
-        img.src = "img_noite.jpg"
+        img.src = "IMAGES/img_noite.jpg"
         document.body.style.background = "rgb(57,69,79)"
     }
 }
